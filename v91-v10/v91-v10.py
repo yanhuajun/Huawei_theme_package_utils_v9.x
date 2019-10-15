8fb745d9021fd6fda829c937a3e44c9de0acdff4
燥起来吧小火苗 <756762990@qq.com>
2019-10-15 11:37:24 +0800

    update ,增加.9图处理

--- v91-v10/v91-v10.py
+++ v91-v10/v91-v10.py
@@ -8,6 +8,7 @@ import fileinput
 import chardet
 import re
 import platform
+from PIL import Image
 
 isWindows = False
 
@@ -648,7 +649,26 @@ normalConfig_dic = {
 	'hwvoipservice_normalConfig_v91_v10':hwvoipservice_normalConfig_v91_v10
 }
 
+dot9picList = [
+#  constacts
+	{'input':'constacts/daier_call_btn_normal.png','output':'com.android.contacts/framework-res-hwext/res/drawable-xxhdpi/daier_call_btn_normal.png'},
+	{'input':'constacts/daier_call_btn_press.png','output':'com.android.contacts/framework-res-hwext/res/drawable-xxhdpi/daier_call_btn_press.png'},
+	{'input':'constacts/dialpad_background_drawable.9.png','output':'com.android.contacts/framework-res-hwext/res/drawable-xxhdpi/dialpad_background_drawable.9.png'},
+	{'input':'constacts/header_background4.9.png','output':'com.android.contacts/framework-res-hwext/res/drawable-xxhdpi/header_background4.9.png'},
 
+# sms
+	{'input':'sms/message_location_pop_send_bg.9.png','output':'com.android.sms/framework-res-hwext/res/drawable-xxhdpi/message_location_pop_send_bg.9.png'},
+	{'input':'sms/message_location_pop_send_bg.9.png','output':'com.android.sms/framework-res-hwext/res/drawable-xxhdpi/message_location_pop_send_bg.9.png'},
+	{'input':'sms/message_pop_rcs_favorite_bg.9.png','output':'com.android.sms/framework-res-hwext/res/drawable-xxhdpi/message_pop_rcs_favorite_bg.9.png'},
+	{'input':'sms/message_pop_rcs_send_bg.9.png','output':'com.android.sms/framework-res-hwext/res/drawable-xxhdpi/message_pop_rcs_send_bg.9.png'},
+	{'input':'sms/message_slide_pop_incoming_bg.9.png','output':'com.android.sms/framework-res-hwext/res/drawable-xxhdpi/message_slide_pop_incoming_bg.9.png'},
+	{'input':'sms/encrypted_message_pop_send_bg.9.png','output':'com.android.sms/framework-res-hwext/res/drawable-xxhdpi/encrypted_message_pop_send_bg.9.png'},
+	{'input':'sms/message_attachment_preview_bg.9.png','output':'com.android.sms/framework-res-hwext/res/drawable-xxhdpi/message_attachment_preview_bg.9.png'},
+	{'input':'sms/message_pop_favorite_bg.9.png','output':'com.android.sms/framework-res-hwext/res/drawable-xxhdpi/message_pop_favorite_bg.9.png'},
+	{'input':'sms/message_pop_send_bg.9.png','output':'com.android.sms/framework-res-hwext/res/drawable-xxhdpi/message_pop_send_bg.9.png'},
+	{'input':'sms/message_slide_pop_send_bg.9.png','output':'com.android.sms/framework-res-hwext/res/drawable-xxhdpi/message_slide_pop_send_bg.9.png'},
+
+]
 
 # 查询所有配置文件位置列表 
 def listConfigFileArray():
@@ -754,7 +774,7 @@ def doTransform( filePath  , configParam ):
 			print '----------'
 	configFileContent+="\n</resources>\n"
 	print configFileContent
-	# open(filePath , 'w').write(configFileContent)
+	open(filePath , 'w').write(configFileContent)
 	print '准备执行文件写入  ，文件为：' + filePath
 		
 
@@ -795,9 +815,110 @@ def checkIcons():
 			print '需补充:' + tmpPath
 			queshipicArr.append(tmpPath)
 
-def checkdot9pic():
-	pass
 
+def mkdir(path):
+    if not os.path.isdir(path):
+        mkdir(os.path.split(path)[0])
+    else:
+        return
+    os.mkdir(path)
+
+def checkdot9pic():
+	for item in dot9picList:
+		inputFilePath = os.path.join(os.getcwd(),item['input'])
+		outFilePath = os.path.join(currentProjectFilePath , item['output'] )
+		if not os.path.exists(inputFilePath):
+			print '模板点9文件出现问题 ， path:%s' % inputFilePath
+			continue
+		if os.path.exists(outFilePath):
+			print '点9文件已存在 ， path:%s , 已跳过处理...' %(  outFilePath )
+			continue
+
+		arr = outFilePath.split('/')
+		tmpPath = ''
+		if isWindows:
+			tmpPath = '\\' + arr[len(arr)-1]
+		else:
+			tmpPath = '/' + arr[len(arr)-1]
+		tmpPath = outFilePath.replace(tmpPath  ,'');
+		if not os.path.exists(tmpPath) :
+			mkdir(tmpPath)
+		if isWindows:
+			inputFilePath = inputFilePath.replace('/','\\',99)
+			outFilePath = outFilePath.replace('/','\\',99)
+		changeDot9PngColor(inputFilePath  ,outFilePath , Hex_to_RGB('#006dc3'))
+
+
+
+
+
+
+ 
+# RGB格式颜色转换为16进制颜色格式
+def RGB_to_Hex(rgb):
+	RGB = rgb.split(',')            # 将RGB格式划分开来
+	color = '#'
+	for i in RGB:
+		num = int(i)
+		# 将R、G、B分别转化为16进制拼接转换并大写  hex() 函数用于将10进制整数转换成16进制，以字符串形式表示
+		color += str(hex(num))[-2:].replace('x', '0').upper()
+	print(color)
+	return color
+ 
+ 
+# 16进制颜色格式颜色转换为RGB格式
+def Hex_to_RGB(hex):
+	r = int(hex[1:3],16)
+	g = int(hex[3:5],16)
+	b = int(hex[5:7], 16)
+	rgb = str(r)+','+str(g)+','+str(b)
+	print(rgb)
+	return rgb
+
+
+def changeDot9PngColor(inputPic , outputPic ,afterColor):
+	img = Image.open(inputPic) 
+	imageWidth = img.width 
+	imageHeight = img.height
+	print 'imgwidth:%s , imgheight:%s' %(imageWidth, imageHeight);
+	
+	for i in xrange(imageWidth):
+		for j in xrange(imageHeight):
+			r,g,b,a= img.getpixel((i,j))
+			if a == 0:
+				continue
+			b=int(afterColor.split(',')[2])
+			g=int(afterColor.split(',')[1])
+			r=int(afterColor.split(',')[0])
+			img.putpixel((i,j), (r,g,b,a)) 
+	if imageWidth <=20 and imageHeight <=20:
+		img.save(outputPic)
+		img.show()
+		return 
+		
+	img2 = Image.new('RGBA',(imageWidth + 1,imageHeight +1)  ,(0,0,0))
+	box1 = (0,0,imageWidth,imageHeight)
+	region = img.crop(box1)
+	img2.paste(region,(1,1))
+
+	for i in xrange(imageWidth + 1):
+		for j in xrange( imageHeight + 1):
+			# print 'i:%d , j:%d' % (i,j)
+			if i == 0 or i == imageWidth or j ==0 or j == imageHeight:
+				if imageWidth > 20 and imageHeight > 20 and ( (i == imageWidth/2 and j == 0) or (i == 0 and j == imageHeight/2) or ( (i >imageWidth/6 and i < imageWidth / 6 * 5) and j == imageHeight) or ( i == imageWidth and j > imageHeight/10 and j< imageHeight/10*9 ) ) :
+					b = 0
+					g = 0
+					r = 0
+					a = 255
+				else:
+					b = 0
+					g = 0
+					r = 0
+					a = 0
+				img2.putpixel((i,j), (r,g,b,a)) 
+
+	img2.save(outputPic)
+	img2.show()
 
 
 
@@ -815,8 +936,9 @@ print '处理文件夹为:' + currentProjectFilePath
 print '\n'
 # 判断是否windows
 print  '当前系统为：' + platform.system()
-if platform.system().find(r'windows' ,re.I):
+if platform.system() == 'Windows' or platform.system() == 'windows':
 	isWindows = True
+
 if isWindows:
 	print 'windows环境'
 else:
@@ -863,3 +985,4 @@ print '==============checkdot9pic============== '
 print '==============完成替换  ，以下打印图片缺失或替换列表 （icons， .9图）============== ' 
 for item in queshipicArr :
 	print item
+
